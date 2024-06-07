a988e77046064201d77e7840a150e167c9b586f1
Eike <eikek@posteo.de>
2024-06-07 16:41:54 +0200

    Update htmx to 1.9.12

--- project/HtmxSourceGeneratorPlugin.scala
+++ project/HtmxSourceGeneratorPlugin.scala
@@ -27,7 +27,7 @@ object HtmxSourceGeneratorPlugin extends AutoPlugin {
 
   val htmxSettings = Seq(
     htmxRepositoryUrl := "https://github.com/bigskysoftware/htmx",
-    htmxRepositoryRef := "v1.9.11",
+    htmxRepositoryRef := "v1.9.12",
     htmxRepositoryTarget := (Compile / Keys.target).value / "htmx-repo",
     htmxReferencePath := "www/content/reference.md",
     htmxGenerateSettings := HtmxSettings(),
