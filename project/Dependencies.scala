18b4ae490a006b60246ba748f0c7a66c7c32550e
eikek-scala-steward <eikek-scala-steward@users.noreply.github.com>
2025-01-22 06:16:58 +0000

    Update munit to 1.1.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -9,7 +9,7 @@ object Dependencies {
     val htmx = "2.0.4"
     val http4s = "0.23.30"
     val http4sScalatags = "0.25.2"
-    val munit = "1.0.4"
+    val munit = "1.1.0"
     val munitScalaCheck = "1.0.0"
     val munitCatsEffect = "2.0.0"
     val scalatags = "0.13.1"
